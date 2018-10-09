6d3c73fc500c620df6375f2a2bf29f17a05fef7f
[Copelia] <iamcorallium@gmail.com>
2018-10-09 12:06:48 -0500

    Rutas condicionadas

--- src/main.js
+++ src/main.js
@@ -12,6 +12,14 @@ const router = new VueRouter({
   routes
 })
 
+router.beforeEach((to, from, next) => {
+  let currentUser = firebase.auth().currentUser
+  let requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  if (requiresAuth && !currentUser) next('login')
+  else if (!requiresAuth && currentUser) next('home')
+  else next()
+})
+
 new Vue({ // eslint-disable-line no-new
   router,
   el: '#app',
